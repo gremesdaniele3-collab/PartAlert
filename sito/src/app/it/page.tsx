ec9f2db5a7e3a3fb7cda606342e7a6605d20d060
gremesdaniele3-collab <gremesdaniele3@gmail.com>
2026-08-04 23:51:18 +0200

    Update page.tsx

--- sito/src/app/it/page.tsx
+++ sito/src/app/it/page.tsx
@@ -3,5 +3,5 @@ import catalog from "../../../data/catalog.json";
 import MainPage from "./MainPage";
 
 export default function Page() {
-  return <MainPage catalog={catalog} />;
+  return <MainPage catalog={catalog as any} />;
 }
